7549ec116e0d4f72eb261db6942de4f92d81341f
ViKuT <252881749+BRO-BESTIE15@users.noreply.github.com>
2026-07-25 15:27:23 +0530

    Added: exercise03.py && git push
    q
    

---
+++ exercise/exercise03.py
@@ -0,0 +1,7 @@
+"""Display only those characters which are present at an even index number in given string."""
+
+original_word = "bro_bestie15"
+
+print(f"Printing orginal string: {original_word}")
+
+print(f"Printing even index characters0qq: {original_word[0::2]}")
